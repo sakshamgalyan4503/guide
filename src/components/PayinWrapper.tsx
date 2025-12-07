9a4415990dfd803b5995f6fcbc810233ccc119f3
Saksham Galyan <146658307+Sakshamgalyan@users.noreply.github.com>
2025-12-07 22:19:42 +0530

    fix

--- src/components/PayinWrapper.tsx
+++ src/components/PayinWrapper.tsx
@@ -3,7 +3,6 @@ import React, { Suspense } from 'react';
 import PayinComp from './payinComp';
 
 export default function PayinWrapper({ src }: { src: string }) {
-   console.log('PayinWrapper mounted with src:', src);
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <PayinComp src={src} />
